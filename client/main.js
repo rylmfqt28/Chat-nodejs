cebb94495b65fd892bde32347ed1cd814a063dac
Rylmar Quispe <rylm.fqt28@gmail.com>
2021-02-20 00:46:17 -0400

    Se implemento el envio y recepcion de mensajes

--- client/main.js
+++ client/main.js
@@ -15,5 +15,19 @@ function render(data){
         `);
     }).join(' ');
 
-    document.getElementById('messages').innerHTML = html;
+    var div_mgs = document.getElementById('messages');
+    div_mgs.innerHTML = html;
+    div_mgs.scrollTop = div_mgs.scrollHeight;
+}
+
+function addMessage(e){
+    var message = {
+        nickname: document.getElementById('nickname').value,
+        text: document.getElementById('text').value
+    };
+
+    document.getElementById('nickname').style.display = 'none';
+    socket.emit('add-message', message);
+
+    return false;
 }
--- server/index.js
+++ server/index.js
@@ -18,6 +18,11 @@ var messages = [{
 io.on('connection', function(socket){
     console.log('New machine connected IP: ' + socket.handshake.address);
     socket.emit('messages', messages);
+
+    socket.on('add-message', function(data){
+        messages.push(data);
+        io.sockets.emit('messages', messages);
+    });
 });
 
 server.listen(6677, function(){
